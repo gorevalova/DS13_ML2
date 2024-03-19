2766e87d25890f9e884e84e15d65de6a0eff4fec
gorevalova <82091885+gorevalova@users.noreply.github.com>
2024-03-19 09:03:17 +0300

    Add files via upload

---
+++ EDA.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def eda_page():
+    st.title('Exploratory Data Analysis')
+    st.write('Страница для анализа данных...')
---
+++ about.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def about_page():
+    st.title('О нас')
+    st.write('Дополнительная информация о нас здесь...')
