7913c1831547f431dfac5cd55af2ecec4392b0ae
gorevalova <82091885+gorevalova@users.noreply.github.com>
2024-03-21 14:15:29 +0300

    Add files via upload
    изменение диапазона длительности

--- model_selection.py
+++ model_selection.py
@@ -8,7 +8,7 @@ def select_page():
         track_name = st.text_input('Название трека')
         acousticness = st.slider('Акустичность', min_value=0.0, max_value=1.0, value=0.2, step=0.01)
         danceability = st.slider('Танцевальность', min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-        duration_ms = st.slider('Продолжительность', min_value=0.0, max_value=40000.0, value=500.0, step=0.01)
+        duration_ms = st.slider('Продолжительность', min_value=0.0, max_value=4000.0, value=500.0, step=0.01)
         energy = st.slider('Энергичность', min_value=0.0, max_value=1.0, value=0.6, step=0.01)
         instrumentalness = st.slider('Инструментальность', min_value=0.0, max_value=1.0, value=0.2, step=0.001)
         key = st.selectbox('Тональность', ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'))
@@ -21,34 +21,36 @@ def select_page():
 
         if st.button('Предсказать'):
             # Обработка введенных параметров
+
             mode = 1 if mode == 'Мажор' else -1
             track_name = int(len(track_name))
 
             data = {
-                'track_name': [track_name],
-                'acousticness': [acousticness],
-                'danceability': [danceability],
-                'duration_ms': [duration_ms],
-                'energy': [energy],
-                'instrumentalness': [instrumentalness],
-                'key': [key],
-                'liveness': [liveness],
-                'loudness': [loudness],
-                'mode': [mode],
-                'speechiness': [speechiness],
-                'tempo': [tempo],
-                'valence': [valence]
+                'track_name': track_name,
+                'acousticness': acousticness,
+                'danceability': danceability,
+                'duration_ms': duration_ms,
+                'energy': energy,
+                'instrumentalness': instrumentalness,
+                'key': key,
+                'liveness': liveness,
+                'loudness': loudness,
+                'mode': mode,
+                'speechiness': speechiness,
+                'tempo': tempo,
+                'valence': valence
             }
 
-            user_input = pd.DataFrame(data)
+            df = pd.DataFrame(data, index=[0])
 
             with open('data/final_model_test.pkl', 'rb') as file:
                 model = pickle.load(file)
 
             # Предсказание музыкального жанра
-            prediction = model.predict(user_input)
+            prediction = model.predict(df)[0]
 
-            prediction_genre_clean = prediction[0].item().replace('[', '').replace(']', '').replace("'", "")
+            # prediction_genre_clean = prediction
+            prediction_genre_clean = prediction[0].replace('[', '').replace(']', '').replace("'", "")
 
             # Вывод предсказанного музыкального жанра
             st.write(f"Предсказанный музыкальный жанр: {prediction_genre_clean}")
